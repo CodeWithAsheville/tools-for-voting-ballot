f4b56223bae6d8cbb3a16e29c5c83465f1bca8fb
Patrick Conant <patrick@prcapps.com>
2016-10-08 13:08:16 -0400

    Updates

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 
 
+
+
+var TFVB = {};
+
+TFVB.first_name = false;
+TFVB.last_name = false;
+TFVB.processVoterName = function(){};
+
+$("#enter-name").submit(TFVB.processVoterName);
+
+$("#first-name").change(function(){
+	alert('go', $(this).val() );
+});
